a5444fea14d9914ddf0748155c6e36464ca65e24
Anastasiya Garbuz <36642340+StationToStation@users.noreply.github.com>
2018-02-26 18:30:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,46 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.arr=[];
+    this.comparator=function compareNumbers(a,b)
+    {
+      if (a>b)return 1;
+      if (a<b) return -1;
+      return 0;
+    }
   }
 
   add(element) {
-    // your implementation
+    this.arr.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.arr[index];
   }
 
   get length() {
-    // your implementation
+    return this.arr.length;
   }
 
   toArray() {
-    // your implementation
+    return this.arr;
   }
 
   sort(indices) {
-    // your implementation
+    let mas2 = [];
+    indices.sort();
+    for (let i = 0; i < indices.length; i++) {
+        mas2.push(this.arr[indices[i]]);
+    }
+    mas2.sort(this.comparator);
+    for(let i = 0; i < indices.length; i++) {
+        this.arr[indices[i]] = mas2[i];
+    }
   }
 
+ 
   setComparator(compareFunction) {
-    // your implementation
+    this.comparator = compareFunction;
+
   }
 }
 
